896fbcfe0b6d3f62e8e8badb2c0c0601460e1f68
Michał Trąbski <michaltrabskiport@gmail.com>
2020-09-17 14:13:09 +0200

    added theme

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,42 @@ import Home from "./pages/home";
 import Login from "./pages/login";
 import Signup from "./pages/signup";
 
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#757ce8",
+      main: "#844677",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
+  },
+});
+
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Navbar />
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
 
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/login" component={Login}></Route>
-            <Route path="/signup" component={Signup}></Route>
-          </Switch>
-        </div>
-      </Router>
-    </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home}></Route>
+              <Route path="/login" component={Login}></Route>
+              <Route path="/signup" component={Signup}></Route>
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
